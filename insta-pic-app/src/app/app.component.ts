96621839dcd2f4f06617ba4b38dbd45e0d825810
jjzapata <jjzapata@bancolombia.com.co>
2025-03-17 20:19:33 -0500

    creating new components

--- insta-pic-app/src/app/app.component.ts
+++ insta-pic-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./pages/login/login.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
