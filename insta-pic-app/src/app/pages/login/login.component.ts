784622993a2377a962e1da2dacda09bf3ce69d42
jjzapata <jjzapata@bancolombia.com.co>
2025-03-27 17:22:00 -0500

    adding forms

--- insta-pic-app/src/app/pages/login/login.component.ts
+++ insta-pic-app/src/app/pages/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  router = inject(Router)
+
+  fb = inject(FormBuilder);
+  loginForm = this.fb.group({
+    username:['', [Validators.required, Validators.minLength(6)]],
+    password:['', [Validators.required]]
+  })
+
+
+
+  onLogin(){
+    if(this.loginForm.invalid){
+      alert('Diligencie todos los campos');
+      return;
+    }
+
+    const {username, password } = this.loginForm.value;
+
+    const userSrt = localStorage.getItem(username!);
+
+    if (!userSrt){
+      alert('Ingreso no valido');
+      return;
+    }
+
+    const user = JSON.parse(userSrt);
+
+    if(password!==user.password){
+
+      alert('Ingreso no valido');
+      return;
+    }
+    this.router.navigateByUrl('home');
+
+  }
 
 }
--- insta-pic-app/src/app/pages/sign-up/sign-up.component.ts
+++ insta-pic-app/src/app/pages/sign-up/sign-up.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
@@ -11,9 +12,40 @@ export class SignUpComponent {
 
   router = inject(Router);
 
+  fb = inject(FormBuilder);
+
+  passwordsMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const rePassword = control.get('rePassword')?.value;
+    return password === rePassword ? null : { passwordsMismatch: true };
+
+  };
+
+  signUpForm = this.fb.group({
+    name:['', [Validators.required, Validators.minLength(6)]],
+    username:['', [Validators.required]],
+    email:['', [Validators.email]],
+    password:['', [Validators.required]],
+    rePassword:['']
+  }, { validators: this.passwordsMatchValidator })
+
 
   onRegistry(){
+
+    if(this.signUpForm.invalid){
+      alert('Diligencie todos los campos');
+      return;
+    }
+
+    const {rePassword:_, ...user} = this.signUpForm.value;
+
+    localStorage.setItem(user.username!, JSON.stringify(user));
+
     this.router.navigateByUrl('home');
+
   }
+
+
+
 
 }
